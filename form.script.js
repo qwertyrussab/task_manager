433be1eee928da1b9a0a2052fa898a29b5753f5c
qwertyrussab <slava.nefedov.02@mail.ru>
2025-09-13 11:47:28 +0300

    more and more logic

--- form.script.js
+++ form.script.js
@@ -1,8 +1,7 @@
 const form = document.querySelector (".form")
+// let counter = 1
 
 
-form.addEventListener("submit", function(event){
-   event.preventDefault()
 
    const table = document.createElement('table')
    table.classList.add("table", "table-striped", "mt-4")
@@ -14,34 +13,49 @@ form.addEventListener("submit", function(event){
                 <th>Название</th>
                 <th>Описание</th>
                 <th>Приоритет</th>
+                <th>удалить</i></th>
             </tr>
         </thead>
         <tbody></tbody>
     `
+
+form.addEventListener("submit", function(event){
+   event.preventDefault()
+
+    
+
     document.body.appendChild(table)
 
     const title = document.querySelector("#inputTitle3").value
     const description = document.querySelector("#inputdesc3").value
     const priority = form.querySelector("input[name=gridRadios]:checked").nextElementSibling.innerText
 
-    if(!title){
-        alert ("Введите название задачи!!!!")
-    }
-    if(!description){
-        alert ("Введите описание задачи!!!!")
-    }
     const tbody = table.querySelector('tbody')
     const row = document.createElement('tr')
 
     row.innerHTML = `
-        <td>1</td> 
+        <td></td> 
         <td>${title}</td>
         <td>${description}</td>
         <td>${priority}</td>
+        <td><button class="delete"><i class="fa-solid fa-trash-can"></button></td>
             
     `
     tbody.appendChild(row)
 
+    function updateNumbers(){
+        const rows = tbody.querySelectorAll('tr')
+        rows.forEach((row,index)=>{
+            row.querySelector('td').innerText = index + 1
+        })
+    }
+
+    row.querySelector('.delete').addEventListener('click', function(){
+        row.remove()
+        updateNumbers()
+    })
+
+    updateNumbers()
 
     form.reset() //ВНИЗУ
 })
